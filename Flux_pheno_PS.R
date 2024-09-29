9e936a88e7f5f827b525309ed660e861bc2a556f
psharma-gif <psharma2@nd.edu>
2024-09-29 13:43:13 -0400

    plotted NEE on recent warm winter and past years to visualize how it has changed.

--- Flux_pheno_PS.R
+++ Flux_pheno_PS.R
@@ -11,27 +11,8 @@ library(hrbrthemes)
 library(htmltools)
 # load data, name by site ID
 
-# Willow Creek, US-WCr
-# 2024
-# FLUXNET data not available; we have HH data only
-data_WCr_2024 <- read.csv(here("data/US-WCr_HH_202401010000_202406190000.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = substr(TIMESTAMP_START, 1, 4),
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Hour = substr(TIMESTAMP_START, 9, 10),
-         Minute = substr(TIMESTAMP_START, 11, 12),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day,
-                                              paste(Hour, Minute, sep=":")),
-                                        format = "%Y %m %d %H:%M")),
-         Date = make_date(Year, Month, Day),
-         Time = format(Datetime, format = "%H:%M"),
-         DoY = yday(Datetime)) %>%
-  group_by(Date) %>%
-  summarise(across(where(is.numeric), mean, na.rm = TRUE))
 
-# Sylvania, US-Syv
+#####Sylvania, US-Syv####
 # 2001 - 2021, FLUXNET daily means available
 data_Syv_01_21 <- read.csv(here("data/AMF_US-Syv_FLUXNET_SUBSET_DD_2001-2021_3-5.csv")) %>%
   mutate_if(is.logical, ~as.numeric(.)) %>%
@@ -47,46 +28,44 @@ data_Syv_01_21 <- read.csv(here("data/AMF_US-Syv_FLUXNET_SUBSET_DD_2001-2021_3-5
 data_Syv_2002 <- read.csv(here("data/AMF_US-Syv_FLUXNET_SUBSET_DD_2001-2021_3-5.csv")) %>%
   mutate_if(is.logical, ~as.numeric(.)) %>%
   mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP, 1, 4)),  # Ensure 'Year' is numeric
+  mutate(Year = as.numeric(substr(TIMESTAMP, 1, 4)), 
          Month = substr(TIMESTAMP, 5, 6),
          Day = substr(TIMESTAMP, 7, 8),
          Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
          Date = make_date(Year, Month, Day),
          DoY = yday(Datetime)) %>%
-  filter(Year == 2002)  # Filter for years 2020 to 2024
+  filter(Year == 2002)  
 
 
 data_Syv_2016 <- read.csv(here("data/AMF_US-Syv_FLUXNET_SUBSET_DD_2001-2021_3-5.csv")) %>%
   mutate_if(is.logical, ~as.numeric(.)) %>%
   mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP, 1, 4)),  # Ensure 'Year' is numeric
+  mutate(Year = as.numeric(substr(TIMESTAMP, 1, 4)), 
          Month = substr(TIMESTAMP, 5, 6),
          Day = substr(TIMESTAMP, 7, 8),
          Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
          Date = make_date(Year, Month, Day),
          DoY = yday(Datetime)) %>%
-  filter(Year == 2016)  # Filter for years 2020 to 2024
+  filter(Year == 2016)  
 
 
 data_Syv_2021 <- read.csv(here("data/AMF_US-Syv_FLUXNET_SUBSET_DD_2001-2021_3-5.csv")) %>%
   mutate_if(is.logical, ~as.numeric(.)) %>%
   mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP, 1, 4)),  # Ensure 'Year' is numeric
+  mutate(Year = as.numeric(substr(TIMESTAMP, 1, 4)),  
          Month = substr(TIMESTAMP, 5, 6),
          Day = substr(TIMESTAMP, 7, 8),
          Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
          Date = make_date(Year, Month, Day),
          DoY = yday(Datetime)) %>%
-  filter(Year == 2021)  # Filter for years 2020 to 2024
+  filter(Year == 2021)  
 
-
-# Set up the plotting area
+#Plotting Syv NEE
 plot(data_Syv_2016$DoY, data_Syv_2016$NEE_VUT_25, 
      type = "o", col = "black", 
      xlab = "DOY", ylab = "NEE", 
 , main = "SYV:NEE", pch=16)
 
-# Add the 2020-2024 data to the plot using lines()
 points(data_Syv_2002$DoY, data_Syv_2002$NEE_VUT_25, 
        col = "red", type = "o", pch=16)
 
@@ -94,15 +73,12 @@ points(data_Syv_2021$DoY, data_Syv_2021$NEE_VUT_25,
        col = "purple", type = "o",pch=16)
 
 
-# Optional: Add a legend to distinguish the datasets
 legend("topleft", legend = c("2016", "2002", "2021"),
        col = c("black", "red", "purple"), lty = 1)
 
-
+#######US-Willow Creek (WCr) ########
 library(dplyr)
-WCr <- read.csv("data/AMF_US-WCr_BASE_HH_30-5.csv", header = T)
-# Sylvania, US-Syv
-# 2001 - 2021, FLUXNET daily means available
+#loaded .csv file of willow creek and also averaged out half hourly data to daily.
 data_WCr_01_21 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
   mutate_if(is.logical, ~as.numeric(.)) %>%
   mutate(across(everything(), ~na_if(., -9999))) %>%
@@ -112,125 +88,34 @@ data_WCr_01_21 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
          Datetime = as.POSIXct(strptime(paste(Year, Month, Day),
                                         format = "%Y %m %d")),
          Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime))
+         DoY = yday(Datetime))%>%
+  group_by(Date) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE))
 
-data_WCr_2020_2024 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP_START, 1, 4)),  # Ensure 'Year' is numeric
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
-         Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime)) %>%
-  filter(Year >= 2020 & Year <= 2024)  # Filter for years 2020 to 2024
+data_WCr_2001_2004 <- data_WCr_01_21 %>%
+  filter(Date >= as.Date("2001-01-01") & Date <= as.Date("2004-12-31"))
 
-data_WCr_2016_2020 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP_START, 1, 4)),  # Ensure 'Year' is numeric
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
-         Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime)) %>%
-  filter(Year >= 2016 & Year <= 2020)  # Filter for years 2020 to 2024
+data_WCr_2020_2024 <- data_WCr_01_21 %>%
+  filter(Date >= as.Date("2020-01-01") & Date <= as.Date("2024-12-31"))
 
-data_WCr_2023 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP_START, 1, 4)),  # Ensure 'Year' is numeric
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
-         Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime)) %>%
-  filter(Year == 2023)  # Filter for years 2020 to 2024
-
-data_WCr_2022 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP_START, 1, 4)),  # Ensure 'Year' is numeric
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
-         Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime)) %>%
-  filter(Year == 2022)  # Filter for years 2020 to 2024
+data_WCr_2016_2020 <- data_WCr_01_21 %>%
+  filter(Date >= as.Date("2016-01-01") & Date <= as.Date("2020-12-31"))
 
 
 
-par(mfrow=c(2,1))
-# Initialize the plot
-plot(data_WCr_2023$DoY, data_WCr_2023$NEE_PI, 
-     type = "l", col = "blue", 
-     xlab = "Datetime", ylab = "NEE_PI", 
-     ylim = c(-70,70))
-
-# Add the 2020-2024 data to the plot
-plot(data_WCr_2022$DoY, data_WCr_2022$NEE_PI, 
-     col = "red", type = "l", 
-     ylim = c(-70,70))
-)
-
-
-
-data_WCr_2001 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP_START, 1, 4)),  # Ensure 'Year' is numeric
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
-         Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime)) %>%
-  filter(Year == 2001)  # Filter for years 2020 to 2024
-
-
-data_WCr_2002 <- read.csv(here("data/AMF_US-WCr_BASE_HH_30-5.csv")) %>%
-  mutate_if(is.logical, ~as.numeric(.)) %>%
-  mutate(across(everything(), ~na_if(., -9999))) %>%
-  mutate(Year = as.numeric(substr(TIMESTAMP_START, 1, 4)),  # Ensure 'Year' is numeric
-         Month = substr(TIMESTAMP_START, 5, 6),
-         Day = substr(TIMESTAMP_START, 7, 8),
-         Datetime = as.POSIXct(strptime(paste(Year, Month, Day), format = "%Y %m %d")),
-         Date = make_date(Year, Month, Day),
-         DoY = yday(Datetime)) %>%
-  filter(Year == 2002)  # Filter for years 2020 to 2024
-
-# Set up the plotting area
-plot(data_WCr_2023$DoY, data_WCr_2023$NEE_PI, 
+#Plot NEE of WCr
+plot(data_WCr_2020_2024$DoY, data_WCr_2020_2024$NEE_PI, 
      type = "o", col = "black", 
-     xlab = "Datetime", ylab = "NEE_PI", 
-     ylim = c(-70, 70), main = "NEE_PI for 2016-2020 and 2020-2024")
+     xlab = "Datetime", ylab = "NEE", 
+     main = "Willow Creek: NEE", ylim = c(-30,30))
 
-# Add the 2020-2024 data to the plot using lines()
-points(data_WCr_2001$DoY, data_WCr_2001$NEE_PI, 
+points(data_WCr_2001_2004$DoY, data_WCr_2001_2004$NEE_PI, 
       col = "red", type = "o")
 
-points(data_WCr_2002$DoY, data_WCr_2002$NEE_PI, 
+points(data_WCr_2016_2020$DoY, data_WCr_2016_2020$NEE_PI, 
       col = "purple", type = "o")
 
-# Optional: Add a legend to distinguish the datasets
-legend("topright", legend = c("2023", "2001", "2002"),
-       col = c("black", "red","purple"), lty = 1)
-
-####################
+legend("topright", legend = c("2020-2024", "2001-2004", "2016-2020"),
+       col = c("black", "red","purple"), lty = 1, cex=0.9)
 
 
-# Set up the plotting area
-plot(data_WCr_2023$DoY, data_WCr_2023$TA_1_1_1, 
-     type = "o", col = "black", 
-     xlab = "Datetime", ylab = "NEE_PI", 
-     ylim = c(-70, 70), main = "NEE_PI for 2016-2020 and 2020-2024")
-
-# Add the 2020-2024 data to the plot using lines()
-points(data_WCr_2001$DoY, data_WCr_2001$TA_1_1_1, 
-       col = "red", type = "o")
-
-points(data_WCr_2002$DoY, data_WCr_2002$TA_1_1_1, 
-       col = "purple", type = "o")
-
-# Optional: Add a legend to distinguish the datasets
-legend("topright", legend = c("2023", "2001", "2002"),
-       col = c("black", "red","purple"), lty = 1)
